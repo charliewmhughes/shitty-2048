96007c05c49e9e0cde8774c335353ac36f4a98d5
Charlie <charliewmhughes@gmail.com>
2018-06-16 21:59:20 +1000

    First working commit, no extra bits, just the core game working

--- master/2048_main.py
+++ master/2048_main.py
@@ -7,12 +7,6 @@ actions.draw_background()
 time.sleep(0.001)
 actions.spawn_tile()
 actions.spawn_tile()
-while running:
-    actions.sense_key()
-    actions.draw_background()
-    counter = 0
-    while counter < 17:
-        actions.update_screen()
-        counter +=1
-    
-    time.sleep(0.001)
+while True:
+    actions.update_screen() 
+    actions.key_control()
