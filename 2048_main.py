c56c73df1df53cd5184f5804b01e63184313115b
Charlie Hughes <charliewmhughes@gmail.com>
2018-06-18 10:35:13 +1000

    Stopped Flashing on screen update, looks uglier.

---
+++ 2048_main.py
@@ -0,0 +1,12 @@
+import actions
+import time
+
+running = 1
+
+actions.draw_background()    
+time.sleep(0.001)
+actions.spawn_tile()
+actions.spawn_tile()
+while True:
+    actions.update_screen() 
+    actions.key_control()
